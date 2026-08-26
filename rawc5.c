6d9643ef77b625034da96ccf3492a9180bd0cdbd
walekarrudra007-coder <walekarrudra007@gmail.com>
2026-08-26 13:38:28 +0530

    Add files via upload

---
+++ rawc5.c
@@ -0,0 +1,18 @@
+/*
+program (5)=write a programe to calculate the average of three numbers 
+solution(1)=predefined values
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a=33;
+    int b=47;
+    int c=20;
+    float average;
+
+    average=(a+b+c)/3;
+    printf("\n average=%f",average);
+
+    return 0;
+}
