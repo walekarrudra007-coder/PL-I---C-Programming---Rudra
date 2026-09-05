153b97f9860deb259678e2d4859bb9c0ee6b93ac
walekarrudra007-coder <walekarrudra007@gmail.com>
2026-09-05 20:51:35 +0530

    rawc9.c

---
+++ rawc9.c
@@ -0,0 +1,14 @@
+// program(9)= write a c program to demostrate the use of single-line and multi-line comments
+
+#include<stdio.h>
+int main()
+{
+    //this is a single-line comment...
+
+    /*
+    this is a multi-line comment.
+    it can span across multiple lines.
+    */
+
+    return 0;
+}
